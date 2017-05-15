3c24421af0a13221695e01639685afeb84c86cf0
Justin Gomez and Tyler Allen <justin+tyler@>
2017-05-15 10:26:36 -0700

    create template for start page and character selection page.

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,18 @@
 //------------------User Interface Logic------------------//
 $(function(){
 
+  $("#backStoryColumn").click(function(event){
+    event.preventDefault();
+    $("#startContainer").hide();
+    $("#characterContainer").show();
+  });
+
+  $("#formCharacter").submit(function(event){
+    event.preventDefault();
+    $("#characterContainer").hide();
+    $("#mapContainer").show();
+  });
+
+
 
 });
